46b7981254a2231168fc59eebe3caa011e9dc24a
jaquelinelim <jaqbungan@gmail.com>
2021-04-02 19:39:21 +0800

    commit to push

--- event handler/js/index.js
+++ event handler/js/index.js
@@ -6,3 +6,15 @@ let elnamaPengguna = document.getElementById(namaPengguna); //1) select element
 elnamaPengguna.addEventListener("keyup",function(){
     elnamaPengguna.value = transformUppercase(elnamaPengguna.value);
 });
+
+function checkPassword(){
+    let elkataLaluan = document.getElementById("kataLaluan");
+    let password_val = elkataLaluan.value;
+    if(password_val.length > 5){
+        alert("password check....OK!");
+        else {
+            alert("Password too short!");        }
+    }
+}
+let elkataLaluan = document getElementById("kataLaluan");
+elkataLaluan.onblur = checkPassword;
