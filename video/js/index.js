7324e4c0ff81bdc66fe413842252ceae981cdd78
jaquelinelim <jaqbungan@gmail.com>
2021-04-05 20:46:08 +0800

    846pm

--- video/js/index.js
+++ video/js/index.js
@@ -3,5 +3,10 @@ let video = document.getElementById("myVideo");
 let playBtn = document.getElementById("playBtn");
 playBtn.addEventListener("click", function(){
     video.play();
-    
+
+});
+
+let pauseBtn = document.getElementById("pauseBtn");
+pauseBtn.addEventListener("click", function(){
+
 });
