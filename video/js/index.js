d7734afd0e52dd82a9b0bdaee767f5c02bfc509d
jaquelinelim <jaqbungan@gmail.com>
2021-04-05 20:58:35 +0800

    858pm

--- video/js/index.js
+++ video/js/index.js
@@ -17,4 +17,15 @@ freezeBtn.addEventListener("mousedown", function(){
 });
 freezeBtn.addEventListener("mouseup", function(){
     video.play();
+});
 
+let seekBar = document.getElementById("seekBar");
+seekBar.addEventListener("change", function(){
+    let time = video.duration * (seekBar.value / 100);
+    video.currentTime = time;
+});
+
+seekBar.addEventListener("timeupdate", function(){
+    let vale = (100 / video.duration) * video.currentTime;
+    seekBar.value;
+});
