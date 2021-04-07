bb9e5aad4d8154420d42d637a2906b6a70ddb20a
jaquelinelim <jaqbungan@gmail.com>
2021-04-07 20:40:53 +0800

    2030

--- vuejs/js/index.js
+++ vuejs/js/index.js
@@ -10,6 +10,8 @@ let app2 = new Vue({
     el: "#app2",
     data: {
         message:"Loaded on " + new Date().toLocaleString()
+        linkUrl: "https://bbc.in/2LAnJs8",
+        linkText: "Google is down!"
     }
 
 });
