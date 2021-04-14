ab3f741458073f29d00ca36f0bee6caa0c69ec70
jaquelinelim <jaqbungan@gmail.com>
2021-04-14 14:56:08 +0800

    256

--- tableapp/js/list.js
+++ tableapp/js/list.js
@@ -1,5 +1,32 @@
 let refreshNowBtn = document.getElementById("refreshNow")
 refreshNowBtn.addEventListener("click",function(){
-    alert("GetBooking");
+    GetBooking();
 });
 
+function GetBooking(){
+    let url = 'https://api.sheety.co/0b37e6178bdecca5bed71fee1cb7e07a/tableapp/bookingitems';
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+
+        let bookingNameList = document.getElementById("bookingNameList");
+
+        for(let i = 0; i < json.bookingitems.length; i++){
+            let gName =  json.bookingitems[i].name;
+            let gEmail =  json.bookingitems[i].email;
+            let gPax =  json.bookingitems[i].pax;
+            let gRemarks =  json.bookingitems[i].remarks;
+            let gId =  json.bookingitems[i].id;
+
+            let row = bookingNameList.insertRow(bookingNameList.rows.length);
+            row.insertCell(0).innerHTML = gId;
+            row.insertCell(1).innerHTML = gName;
+            row.insertCell(2).innerHTML = gEmail;
+            row.insertCell(3).innerHTML = gPax;
+            row.insertCell(4).innerHTML = gRemarks;
+            row.insertCell(5).innerHTML = "TBC";
+
+            
+        }
+    });
+}
