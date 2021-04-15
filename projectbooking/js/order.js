31b9864f039e51eaf6ecca818f38e0bce4334d76
jaquelinelim <jaqbungan@gmail.com>
2021-04-15 09:47:36 +0800

    947

---
+++ projectbooking/js/order.js
@@ -0,0 +1,37 @@
+let bookNowBtn = document.getElementById("bookNow");
+bookNowBtn.addEventListener("click",function(){
+    let userName = document.getElementById("userName");
+    let userNameVal = userName.value;
+
+    let userEmail = document.getElementById("userEmail");
+    let userEmailVal = userEmail.value;
+
+    let userPaxVal = document.getElementById("userPax").value;
+    let userRemarksVal = document.getElementById("userRemarks").value;
+
+    BookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal);
+});
+
+function BookNow(userName, userEmail, userPax, userRemarks){
+    let url = 'https://api.sheety.co/0b37e6178bdecca5bed71fee1cb7e07a/tableapp/bookingitems';
+    let body = {
+      bookingitem: {
+          name: userName,
+          email: userEmail,
+          pax: userPax,
+          remarks: userRemarks
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+          "Content-Type": "application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {
+        alert("ID: " + json.bookingitem.id + "," + json.bookingitem.name + " successfully added!");
+    });
+
+}
